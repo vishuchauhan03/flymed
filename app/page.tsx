364627ddb731b9653d2d774b75548d09f50a299e
vishuchauhan03 <vishalchauhan.chauhan66@gmail.com>
2024-02-02 01:09:31 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+// import { Link, Button } from "@nextui-org/react";
+
+
+export default function Home() {
+	return (
+		<>
+		<h1>anuj</h1>
+		{/* <div>
+			<Button
+			href="https://github.com/nextui-org/nextui"
+			as={Link}
+			color="primary"
+			showAnchorIcon
+			variant="solid"
+			>
+			Button Link
+			</Button>	
+		</div> */}
+		</>
+	);
+}
